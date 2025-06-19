1491d0dc2065066b7c3ed647a2a693a5fe371005
Matsumoto Kotaro <e235737@eve.u-ryukyu.ac.jp>
2025-06-19 11:39:34 +0900

    自動修正

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 export function debugLog(plugin: { settings?: { debugLogging?: boolean } } | undefined, ...args: any[]): void {
     if (plugin && plugin.settings?.debugLogging) {
-        // eslint-disable-next-line no-console
+         
         console.log(...args);
     }
 }
