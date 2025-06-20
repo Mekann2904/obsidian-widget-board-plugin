12daaec1bba6d4e614c26853f91be3a56ae8208d
Mekann2904 <e235737@ie.u-ryukyu.ac.jp>
2025-06-21 02:08:36 +0900

    chore: type chart.js usage

--- src/modal.ts
+++ src/modal.ts
@@ -118,7 +118,7 @@ export class WidgetBoardModal {
         // CSS containmentを適用してレイアウト計算の影響範囲を限定
         this.modalEl.style.contain = 'layout style paint';
         // Style recalculation対策として不可視要素はスキップ
-        this.modalEl.style.contentVisibility = 'auto';
+        (this.modalEl.style as CSSStyleDeclaration & { contentVisibility?: string }).contentVisibility = 'auto';
     }
 
     /**
